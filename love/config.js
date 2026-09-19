d41cc6e633d46606607ed186a88d425ae71b56cb
aroarable <148499844+aroarable@users.noreply.github.com>
2026-09-19 11:54:42 +0300

    Add files via upload

---
+++ love/config.js
@@ -0,0 +1,6 @@
+/* Kurulum gerekmez. İstekler ntfy.sh üzerinden bu "konu" adına gider.
+   Konu adı tahmin edilemez, başkasına verme.
+   Değiştirmek istersen admin.html ve index.html aynı config.js'i kullanır. */
+window.LOVE_CONFIG = {
+  ntfyTopic: "love-26a277a1b33cc769"
+};
